a79ff6007f180dacbea9a27bbca1928db4883de3
t <t@Tomas>
2024-01-11 12:12:32 -0300

    graphs & benchmarking

---
+++ benchmarking/sort.h
@@ -0,0 +1,10 @@
+#include <time.h>
+#include <stdlib.h> // for srand
+
+#ifndef SORT_H
+#define SORT_H
+
+// Sorts the given array
+void quick_sort(int numbers[], int left, int right);
+
+#endif // PROJECT_QUICK_SORT_H
